75cd66e989d0b936842765dcc6d0f68221f8f7c3
Dustin Hellmann <dustinhellmann@t-online.de>
2019-04-08 17:22:58 +0200

    Added main, added plugins for building fat-jars,

--- src/main/java/helper/GraphParser.java
+++ src/main/java/helper/GraphParser.java
@@ -12,7 +12,8 @@ import java.util.List;
 import java.util.Map;
 
 public class GraphParser {
-    public List<Vertex> readEdgeListFromFile(final String fileName, boolean directed) {
+
+    public static List<Vertex> readEdgeListFromFile(final String fileName, boolean directed) {
         final ArrayList<Vertex> vertices = new ArrayList<Vertex>();
         try {
             final FileReader fileReader = new FileReader(fileName);
@@ -22,40 +23,6 @@ public class GraphParser {
             int nOfVertices = Integer.parseInt(firstLine);
             for (int i = 0; i < nOfVertices; i++) {
                 vertices.add(new Vertex(i));
-            }
-
-            final String delimiter = "\t";
-            String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                final String[] points = currentLine.split(delimiter);
-                final int p1 = Integer.parseInt(points[0]);
-                final int p2 = Integer.parseInt(points[1]);
-
-                if (directed) {
-                    vertices.get(p1).addEdge(new Edge(vertices.get(p1), vertices.get(p2)));
-                    vertices.get(p2).addEdge(new Edge(vertices.get(p2), vertices.get(p1)));
-                } else {
-                    Edge e = new Edge(vertices.get(p1), vertices.get(p2));
-                    vertices.get(p1).addEdge(e);
-                    vertices.get(p2).addEdge(e);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return vertices;
-    }
-
-    public Map<Integer, Vertex> readEdgeListFromFileAsMap(final String fileName, boolean directed) {
-        final Map<Integer, Vertex> vertices = new HashMap<Integer, Vertex>();
-        try {
-            final FileReader fileReader = new FileReader(fileName);
-            final BufferedReader reader = new BufferedReader(fileReader);
-
-            String firstLine = reader.readLine();
-            int nOfVertices = Integer.parseInt(firstLine);
-            for (int i = 0; i < nOfVertices; i++) {
-                vertices.put(i, new Vertex(i));
             }
 
             final String delimiter = "\t";
