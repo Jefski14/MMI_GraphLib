18a31ef3fc5ffb736d8882ed259079ba59dcc4d7
JEFFLAPTOP\Jeff <jerome.fijalkowski@gmail.com>
2019-04-08 17:12:41 +0200

    Added Test for BFS Method

--- src/test/java/algorithm/BFSTest.java
+++ src/test/java/algorithm/BFSTest.java
@@ -9,9 +9,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+
 public class BFSTest {
     @Test
     public void bfsDirectedTest() {
+        final Map<Integer, Boolean> markedMapAfterBFSFromV1 = new HashMap<Integer, Boolean>();
+        markedMapAfterBFSFromV1.put(0, true);
+        markedMapAfterBFSFromV1.put(1, false);
+        markedMapAfterBFSFromV1.put(2, false);
+        markedMapAfterBFSFromV1.put(3, true);
+        markedMapAfterBFSFromV1.put(4, false);
+        markedMapAfterBFSFromV1.put(5, true);
+        markedMapAfterBFSFromV1.put(6, true);
+        markedMapAfterBFSFromV1.put(7, false);
+        markedMapAfterBFSFromV1.put(8, false);
+        markedMapAfterBFSFromV1.put(9, true);
+        markedMapAfterBFSFromV1.put(10, true);
+        markedMapAfterBFSFromV1.put(11, false);
+        markedMapAfterBFSFromV1.put(12, false);
+        markedMapAfterBFSFromV1.put(13, true);
+        markedMapAfterBFSFromV1.put(14, false);
+
         final String path = "src/main/resources/p1/Graph1.txt";
         final boolean directed = false;
         final Map<Integer, Boolean> markedMap = new HashMap<Integer, Boolean>();
@@ -19,6 +38,7 @@ public class BFSTest {
         vertList.stream().forEach(v -> markedMap.put(v.getId(), false));
         final BFS search = new BFS();
         search.breadthFirstSearch(vertList.get(0), markedMap, directed);
-        
+
+        assertEquals(markedMap, markedMapAfterBFSFromV1);
     }
 }
