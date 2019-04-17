a7e95a86cfe2fe620e7dcb168c0b00e76ebd1e6c
JEFFLAPTOP\Jeff <jerome.fijalkowski@gmail.com>
2019-04-17 20:45:32 +0200

    Refined Tests

--- src/test/java/algorithms/PrimMSTTest.java
+++ src/test/java/algorithms/PrimMSTTest.java
@@ -13,9 +13,51 @@ import static org.junit.Assert.assertEquals;
 public class PrimMSTTest {
 
     @Test
-    public void PrimMSTG1Test() {
+    public void PrimMSTG1_2Test() {
         Graph graph = importGraphFromFileAsEdgeList("src/main/resources/p2/G_1_2.txt", false);
         Graph mst = getMST(graph, graph.getVertexList().get(0));
-        assertEquals(mst.totalEdgeCost(), 286.711,0.001);
+        assertEquals(286.711, mst.totalEdgeCost(), 0.001);
+    }
+
+    @Test
+    public void PrimMSTG1_20Test() {
+        Graph graph = importGraphFromFileAsEdgeList("src/main/resources/p2/G_1_20.txt", false);
+        Graph mst = getMST(graph, graph.getVertexList().get(0));
+        assertEquals(29.5493, mst.totalEdgeCost(), 0.001);
+    }
+
+    @Test
+    public void PrimMSTG1_200Test() {
+        Graph graph = importGraphFromFileAsEdgeList("src/main/resources/p2/G_1_200.txt", false);
+        Graph mst = getMST(graph, graph.getVertexList().get(0));
+        assertEquals(3.0228, mst.totalEdgeCost(),0.001);
+    }
+
+//    @Test
+//    public void PrimMSTG10_20Test() {
+//        Graph graph = importGraphFromFileAsEdgeList("src/main/resources/p2/G_10_20.txt", false);
+//        Graph mst = getMST(graph, graph.getVertexList().get(0));
+//        assertEquals(mst.totalEdgeCost(), 286.711,0.001);
+//    }
+
+    @Test
+    public void PrimMSTG10_20Test() {
+        Graph graph = importGraphFromFileAsEdgeList("src/main/resources/p2/G_10_20.txt", false);
+        Graph mst = getMST(graph, graph.getVertexList().get(0));
+        assertEquals(2775.44, mst.totalEdgeCost(),0.0013);
+    }
+
+    @Test
+    public void PrimMSTG10_200Test() {
+        Graph graph = importGraphFromFileAsEdgeList("src/main/resources/p2/G_10_200.txt", false);
+        Graph mst = getMST(graph, graph.getVertexList().get(0));
+        assertEquals(301.552, mst.totalEdgeCost(), 0.001);
+    }
+
+    @Test
+    public void PrimMSTG100_200Test() {
+        Graph graph = importGraphFromFileAsEdgeList("src/main/resources/p2/G_100_200.txt", false);
+        Graph mst = getMST(graph, graph.getVertexList().get(0));
+        assertEquals(27450.6, mst.totalEdgeCost(), 0.018);
     }
 }
