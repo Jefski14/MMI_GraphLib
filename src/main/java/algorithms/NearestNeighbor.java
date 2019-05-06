83431651e3bfd569e2a33d1c030e4ed4a931e1b5
JEFFLAPTOP\Jeff <jerome.fijalkowski@gmail.com>
2019-05-06 14:26:52 +0200

    Added Nearest Neighbor skeleton

---
+++ src/main/java/algorithms/NearestNeighbor.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+import entity.Graph;
+
+/**
+ *
+ */
+public class NearestNeighbor {
+
+    /**
+     * Calculates Round Trip that schould be realtivly optimal
+     * @param graph complete undirected Graph
+     * @return graph that contains round trip
+     */
+    public static Graph calculateRoundTrip(Graph graph) {
+        // Start from any node, take the cheapest edge to the next node until every node has been visited and we're back at start
+        return null;
+    }
+}
