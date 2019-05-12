53efafeb8276d960aaac46e58f0df68b4d90ff1f
JEFFLAPTOP\Jeff <jerome.fijalkowski@gmail.com>
2019-05-12 13:25:50 +0200

    Finished DoubleTree Algorithm

--- src/main/java/algorithms/DoubleTrees.java
+++ src/main/java/algorithms/DoubleTrees.java
@@ -15,34 +15,29 @@ public class DoubleTrees {
     }
 
     public static Graph calculateTour(Graph graph, Vertex start) {
+        // Get Mst (Prim)
         Graph mst = getMST(graph, start);
+        // Initialize marked map for dfs
         Map<Integer, Boolean> marked = new HashMap<>();
         for(Vertex v : mst.getVertexList()) {
             marked.put(v.getId(), false);
         }
-        ArrayList<Edge> tspEdgeList = new ArrayList<>();
 
+        ArrayList<Edge> tspEdgeList = new ArrayList<>();
         Stack<Vertex> stack = new Stack<>();
 
+        // Add first vertex to stack
         stack.push(mst.getVertexList().get(start.getId()));
+        Vertex lastVisitedVertex = start;
 
         while(!stack.empty()) {
             Vertex current = stack.pop();
             if (!marked.get(current.getId())) {
-                // TODO set edge to vertex before
-                Vertex lastInEdgeList;
-                if (tspEdgeList.isEmpty()) { // Start Vertex
-                    lastInEdgeList = mst.getVertexList().get(start.getId());
-                } else {
-                    lastInEdgeList = tspEdgeList.get(tspEdgeList.size() -1).getEnd();
+                if (current.getId() != start.getId()) { // When we're not in the first iteration add edge from last visited to current
+                   tspEdgeList.add(getEdgeWithSpecificEnd(graph.getVertexList().get(lastVisitedVertex.getId()).getAttachedEdges(), current.getId()));
                 }
-                if (tspEdgeList.size() == mst.getVertexList().size() -1) {
-//                    tspEdgeList.add(getEdgeWithSpecificEnd(graph.getVertexList().get(lastInEdgeList.getId()))); // TODO Fall für letzten Knoten
-                } else {
-                    tspEdgeList.add(getEdgeWithSpecificEnd(graph.getVertexList().get(lastInEdgeList.getId()).getAttachedEdges(), current.getId()));
-                }
-
                 marked.put(current.getId(), true);
+                lastVisitedVertex = current;
                 // Add unmarked neighbors to Stack
                 for(Edge e: current.getAttachedEdges()) {
                     if (!marked.get(e.getEnd().getId())) {
@@ -51,10 +46,18 @@ public class DoubleTrees {
                 }
             }
         }
+        // Add Edge back to start
+        tspEdgeList.add(getEdgeWithSpecificEnd(graph.getVertexList().get(lastVisitedVertex.getId()).getAttachedEdges(), start.getId()));
 
         return new Graph(tspEdgeList, true, mst.getVertexList().size());
     }
 
+    /**
+     * Searches for Edge with specific End in given edgelist
+     * @param attachedEdges
+     * @param endpointId
+     * @return Copy of Edge
+     */
     private static Edge getEdgeWithSpecificEnd(List<Edge> attachedEdges, int endpointId) {
         for (Edge e: attachedEdges) {
             if (e.getEnd().getId() == endpointId) {
@@ -62,59 +65,5 @@ public class DoubleTrees {
             }
         }
         throw new RuntimeException("No Edge pointing to vertex with id: " + endpointId + " found");
-    }
-
-    /**
-     * Depth first search (iterative)
-     *
-     * @param vertex   starting vertex
-     * @param marked   map of already marked vertices (should be initialized)
-     * @param directed boolean flag that indicates if the graph is directed
-     */
-    public List<Vertex> iterativeDepthFirstSearch(final Vertex vertex, final Map<Integer, Boolean> marked, boolean directed) {
-        Stack<Vertex> stack = new Stack<>();
-        stack.push(vertex); // Add starting vertex to stack
-
-        //List for output
-        List<Vertex> transition = new ArrayList<>();
-        //Add new vertex with empty edge-list
-        transition.add(new Vertex(vertex.getId()));
-
-        //Index to refer to last added vertex in list
-        int i = 0;
-        while (!stack.empty()) {
-            Vertex current = stack.pop(); // get next vertex (top of stack)
-            if (!marked.get(current.getId())) {
-                marked.put(current.getId(), true); // mark vertex
-                addNeighborsToStack(current, stack, marked, directed);
-
-                transition.add(new Vertex(current.getId()));
-                transition.get(i).getAttachedEdges().add(new Edge(transition.get(i), current));
-                i++;
-            }
-        }
-        return transition;
-    }
-
-    /**
-     * Adds neighbours of given vertex to given stack
-     *
-     * @param vertex   vertex
-     * @param stack    stack
-     * @param marked   marker map
-     * @param directed directed graph flag
-     */
-    private void addNeighborsToStack(final Vertex vertex, Stack<Vertex> stack, final Map<Integer, Boolean> marked, boolean directed) {
-        vertex.getAttachedEdges().forEach(edge -> {
-            if (directed) {
-                if (!marked.get(vertex.getId())) {
-                    stack.push(edge.getEnd()); // add unmarked vertex to stack
-                }
-            } else {
-                if (edge.getStart().equals(vertex) ? !marked.get(edge.getEnd().getId()) : !marked.get(edge.getStart().getId())) {
-                    stack.push(edge.getStart().equals(vertex) ? edge.getEnd() : edge.getStart()); // add unmarked vertex to stack
-                }
-            }
-        });
     }
 }
--- src/test/java/algorithms/DoubleTreesTest.java
+++ src/test/java/algorithms/DoubleTreesTest.java
@@ -3,11 +3,9 @@ package algorithms;
 import entity.Graph;
 import org.junit.Test;
 
-import java.math.BigDecimal;
 
 import static algorithms.DoubleTrees.calculateTour;
 import static helper.GraphParser.importGraphFromFile;
-import static org.junit.Assert.assertEquals;
 
 public class DoubleTreesTest {
 
@@ -18,8 +16,128 @@ public class DoubleTreesTest {
         System.out.println("Starting DoubleTrees");
         Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
         long estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println("NN took: " + estimatedTime + "ms");
+        System.out.println("DT took: " + estimatedTime + "ms");
         System.out.println(tsp.getEdgeList().toString());
-        assertEquals(BigDecimal.valueOf(38.41), tsp.totalEdgeCost());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_10eTest() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_10e.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_12Test() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_12.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_12eTest() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_12e.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_15Test() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_15.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_15eTest() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_15e.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_20Test() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_20.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_30Test() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_30.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_50Test() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_50.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_70Test() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_70.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
+    }
+
+    @Test
+    public void K_100Test() {
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_100.txt", false);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Starting DoubleTrees");
+        Graph tsp = calculateTour(graph); // NN von 9 liefert optimale Tour
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.println("DT took: " + estimatedTime + "ms");
+        System.out.println(tsp.getEdgeList().toString());
+        System.out.println("Total Path Cost: " + tsp.totalEdgeCost());
     }
 }
