5243ddc9d38bf1821a9696177264adfca3fefb95
JEFFLAPTOP\Jeff <jerome.fijalkowski@gmail.com>
2019-05-15 18:55:08 +0200

    B&B Refactoring Part1

--- src/main/java/algorithms/P3/BranchAndBound.java
+++ src/main/java/algorithms/P3/BranchAndBound.java
@@ -1,193 +1,147 @@
 package algorithms.P3;
 
+import entity.Edge;
 import entity.Graph;
-import lombok.Getter;
+import entity.Vertex;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-@Getter
 public class BranchAndBound {
 
-    //Number of vertices in graph
-    private int numberVertices;
-    //Edge list with n+1 edges
-    private int[] finalPath;
-    //keeps track of already visited vertices
-    private boolean[] visited;
-    //Cost of final shortest tour
-    private double finalCost;
-    //Defines whether to stop execution if tour is
-    //more expensive than current best tour
-    private boolean enumerate;
+    private static boolean fullEnumeration = false;
 
-    BranchAndBound(Graph graph, boolean enumerate) {
-        this.numberVertices = graph.getVertexList().size();
-        this.finalPath = new int[numberVertices + 1];
-        this.visited = new boolean[numberVertices];
-        this.finalCost = Double.MAX_VALUE;
-        this.enumerate = enumerate;
-    }
-
-    //Copies temporary solution to final solution
-    //and connects last node with first node
-    private void copyToFinal(int[] curr_path) {
-        if (numberVertices >= 0)
-            System.arraycopy(curr_path, 0, finalPath, 0, numberVertices);
-        finalPath[numberVertices] = curr_path[0];
-    }
-
-    //Function to find the minimum edge cost
-    // having an end at vertex i
-    private Double findFirstMin(Graph g, int i) {
-        Double min = Double.MAX_VALUE;
-        for (int k = 0; k < numberVertices; k++) {
-            Double edgeCost = g.getVertexList().get(i).getAttachedEdges().get(k).getCost();
-            //If edgeCost < min
-            if (edgeCost < min && i != k) {
-                min = edgeCost;
-            }
+    /**
+     * Calculates the cheapest TSP Tour
+     * Warning this can take very long depending on your graph size
+     * @param graph Graph in which to search the tour
+     */
+    public Graph calculateTour(Graph graph) {
+        ArrayList<Integer> unvisitedVertices = new ArrayList<>();
+        for(Vertex v : graph.getVertexList()) { // Clone Vertexlist
+            unvisitedVertices.add(v.getId());
         }
-        return min;
-    }
 
-    //Function to find the second minimum edge cost
-    // having an end at the vertex i
-    private Double findSecondMin(Graph g, int i) {
-        Double first = Double.MAX_VALUE, second = Double.MAX_VALUE;
-        for (int j = 0; j < numberVertices; j++) {
-            if (i == j) {
-                continue;
-            }
-            Double edgeCost = g.getVertexList().get(i).getAttachedEdges().get(j).getCost();
-            if (edgeCost <= first) {
-                second = first;
-                first = edgeCost;
-                //if edgeCost <= second && edgeCost != first
-            } else if (edgeCost <= second && !edgeCost.equals(first))
-                second = edgeCost;
+        ArrayList<Path> allPaths = new ArrayList<>(); // List to save all Paths
+        Graph nnTour = NearestNeighbor.calculateTour(graph);
+        Double upper = nnTour.totalEdgeCost(); // Initialize upper bound with nearest neighbor try
+
+        // Start recursive Depth Search Style building of paths
+        for (Vertex v : graph.getVertexList()) {
+            Path currentPath = new Path();
+            // Copy unvisited Vertex List because its passed down by reference (else it would be changed TODO Test this because maybe stack stuff does it for me)
+            ArrayList<Integer> unvisitedVerticesCopy = (ArrayList<Integer>) unvisitedVertices.clone();
+            unvisitedVerticesCopy.remove(v.getId()); // mark current Vertex v as visited
+            currentPath.addVertex(v.getId(), 0.0); // Add v to Path (first vertex has no cost)
+
+            // Find all paths recursivly
+            upper = buildRecursiveTree(currentPath.clone(), unvisitedVerticesCopy, allPaths, graph.getVertexList(), upper);
         }
-        return second;
+        // Pick out the cheapest Path we found
+        Collections.sort(allPaths);
+        Path cheapest = allPaths.get(0);
+
+        // Build new Graph from found path
+        ArrayList<Edge> tspEdgeList = new ArrayList<>();
+        for (int i = 1; i < cheapest.vertices.size(); i++) {
+            tspEdgeList.add(
+                    new Edge(
+                            new Vertex(cheapest.vertices.get(i-1)), new Vertex(cheapest.vertices.get(i)),
+                                    getCostOfEdge(graph.getVertexList().get(cheapest.vertices.get(i-1)).getAttachedEdges(), cheapest.vertices.get(i)), 0.0));
+        }
+        return new Graph(tspEdgeList, graph.isDirected(), graph.getVertexList().size());
     }
 
     /**
-     * @param g           Graph object to extract vertices and edges from
-     * @param curr_bound  lower bound
-     * @param curr_weight weight of current path
-     * @param level       current level while moving through search tree
-     * @param curr_path   store of current solution, later being copied to finalPath
+     * Recursive depth search function that builds all paths in graph
+     * @param currentPath copy of the current path
+     * @param unvisitedVertices list of vertices that need to be visited
+     * @param allPaths reference of All Path Object in which a newly found path will be written
+     * @param graphVertexList reference of the original graph vertex list to get the cost of any edge
+     * @param upper current upper bound
+     * @return same or improved upper bound
      */
-    private void TSPRec(Graph g, Double curr_bound, Double curr_weight,
-                        int level, int[] curr_path) {
-        // base case is when we have reached level N which
-        // means we have covered all the nodes once
-        if (level == numberVertices) {
-            // check if there is an edge from last vertex in
-            // path back to the first vertex
-            Double edgeCost = g.getVertexList().get(curr_path[level - 1]).getAttachedEdges().get(curr_path[0]).getCost();
-            //if curr_res != 0.0
-            if (edgeCost != 0.0) {
-                // curr_res has the total weight of the
-                // solution we got
-                Double curr_res = curr_weight + edgeCost;
-
-                // Update final result and final path if
-                // current result is better.
-                //if curr_res < finalCost
-                if (curr_res.compareTo(finalCost) < 0) {
-                    copyToFinal(curr_path);
-                    finalCost = curr_res;
-                }
+    private Double buildRecursiveTree(Path currentPath, ArrayList<Integer> unvisitedVertices, List<Path> allPaths, List<Vertex> graphVertexList, Double upper) {
+        // Recursion end case
+        if (unvisitedVertices.size() == 0) { // All vertices are visited -> complete Hamilton Path
+            // Add the edge back to the starting vertex
+            currentPath.addVertex(currentPath.vertices.get(0),
+                    getCostOfEdge(
+                            graphVertexList.get(currentPath.vertices.get(currentPath.vertices.size() - 1)).getAttachedEdges(),
+                            currentPath.vertices.get(0)));
+            if (currentPath.cost < upper || fullEnumeration) { // If its still cheaper
+                upper = currentPath.cost; // Change upper Bound to new upper Bound
+                // Add newly found cheapest path
+                allPaths.add(currentPath);
             }
-            return;
+            // Propagate the newly found bound back up
+            return upper;
         }
 
-        // for any other level iterate for all vertices to
-        // build the search space tree recursively
-        for (int i = 0; i < numberVertices; i++) {
-            // Consider next vertex if it is not same (diagonal
-            // entry in adjacency matrix and not visited
-            // already)
-            Double edgeCost = g.getVertexList().get(curr_path[level - 1]).getAttachedEdges().get(i).getCost();
-            if (edgeCost != 0.0 && !visited[i]) {
-                Double temp = curr_bound;
-                curr_weight = curr_weight + edgeCost;
+        for(Integer i : unvisitedVertices) {
+            // Clone current path because java passes down reference
+            Path currentPathCopy = currentPath.clone();
+            // Add new edge from last edge in path to the new one (i)
+            currentPathCopy.addVertex(i, getCostOfEdge(graphVertexList.get(currentPathCopy.vertices.get(currentPathCopy.vertices.size() - 1)).getAttachedEdges(), i));
+            // Remove the current Vertex from the unvisited list
+            ArrayList<Integer> unvisitedVerticesCopy = (ArrayList<Integer>) unvisitedVertices.clone();
+            unvisitedVerticesCopy.remove(i);
+            // If its still lower than the bound continue down
+            if (currentPath.cost < upper || fullEnumeration)
+                upper = buildRecursiveTree(currentPathCopy, unvisitedVerticesCopy, allPaths, graphVertexList, upper);
 
-                // different computation of curr_bound for
-                // level 2 from the other levels
-                if (level == 1) {
-                    Double i1 = findFirstMin(g, curr_path[level - 1]);
-                    Double i2 = (findFirstMin(g, i) / 2);
-                    Double add = findFirstMin(g, curr_path[level - 1]) + (findFirstMin(g, i) / 2);
-                    curr_bound = curr_bound - add;
-                } else {
-                    Double i1 = findSecondMin(g, curr_path[level - 1]);
-                    Double i2 = findFirstMin(g, i) / 2;
-                    Double add = i1 + i2;
-                    curr_bound = curr_bound - add;
-                }
-
-                // curr_bound + curr_weight is the actual lower bound
-                // for the node that we have arrived on
-                // If current lower bound < final_res, we need to explore
-                // the node further
-                Double add = curr_bound + curr_weight;
-                if (add.compareTo(finalCost) < 0) {
-                    curr_path[level] = i;
-                    visited[i] = true;
-
-                    // call TSPRec for the next level
-                    TSPRec(g, curr_bound, curr_weight, level + 1,
-                            curr_path);
-                }
-
-                // Else we have to prune the node by resetting
-                // all changes to curr_weight and curr_bound
-                Double edgeCost2 = g.getVertexList().get(curr_path[level - 1]).getAttachedEdges().get(i).getCost();
-                curr_weight = curr_weight - edgeCost2;
-                curr_bound = temp;
-
-                // Also reset the visited array
-                Arrays.fill(visited, false);
-                for (int j = 0; j <= level - 1; j++)
-                    visited[curr_path[j]] = true;
-            }
         }
+        return upper;
     }
 
-    public void TSP(Graph g) {
-        int[] curr_path = new int[numberVertices + 1];
+    /**
+     * Searches for Edge with specific Endpoint and returns its cost
+     * @param attachedEdges List of edges to search through
+     * @param endId id of end vertex
+     * @return cost of edge
+     */
+    private static Double getCostOfEdge(List<Edge> attachedEdges, int endId) {
+        for (Edge e: attachedEdges) {
+            if (e.getEnd().getId() == endId) {
+                return e.getCost();
+            }
+        }
+        throw new RuntimeException("No Edge with endpoint found");
+    }
 
-        // Calculate initial lower bound for the root node
-        // using the formula 1/2 * (sum of first min +
-        // second min) for all edges.
-        // Also initialize the curr_path and visited array
-        Double curr_bound = 0.0;
-        Arrays.fill(curr_path, -1);
-        Arrays.fill(visited, false);
-
-        // Compute initial bound
-        for (int i = 0; i < numberVertices; i++) {
-            Double firstMin = findFirstMin(g, i);
-            Double secondMin = findSecondMin(g, i);
-            curr_bound = curr_bound + firstMin + secondMin;
+    private class Path implements Comparable<Path> {
+        Path() {
+            this.vertices = new ArrayList<>();
+            // this.vertcost = new ArrayList<>();
+            this.cost = 0.0;
+        }
+        Path(ArrayList<Integer> verts, double c) {
+            this.vertices = verts;
+            this.cost = c;
         }
 
-        // Rounding off the lower bound to an integer
-        if (curr_bound.equals(BigDecimal.valueOf(1.0))) {
-            curr_bound = (curr_bound / 2) + 1;
-        } else {
-            curr_bound = curr_bound / 2;
+        public void addVertex(int id, Double cost) {
+            this.vertices.add(id);
+            // this.vertcost.add(cost);
+            this.cost += cost;
         }
 
-        // We start at vertex 1 so the first vertex
-        // in curr_path[] is 0
-        visited[0] = true;
-        curr_path[0] = 0;
+        public Path clone() {
+            Path p = new Path();
+            p.vertices = (ArrayList<Integer>) this.vertices.clone();
+            // p.vertcost = (ArrayList<Double>) this.vertcost.clone();
+            p.cost = this.cost;
+            return p;
+        }
 
-        // Call to TSPRec for curr_weight equal to
-        // 0 and level 1
-        TSPRec(g, curr_bound, 0.0, 1, curr_path);
+        ArrayList<Integer> vertices;
+        // ArrayList<Double> vertcost;
+        Double cost;
+
+        @Override
+        public int compareTo(Path p1) {
+            return this.cost.compareTo(p1.cost);
+        }
     }
 }
->>>>>>> 0e647734395c76a7a9ac1e054fdaceed120b255b
--- src/test/java/algorithms/P3/BranchAndBoundTest.java
+++ src/test/java/algorithms/P3/BranchAndBoundTest.java
@@ -8,32 +8,29 @@ import static helper.GraphParser.importGraphFromFile;
 public class BranchAndBoundTest {
 
     @Test
-    public void test() {
-
-        Graph graph = importGraphFromFile("src/main/resources/p3/K_10.txt", false);
-
-        BranchAndBound branchAndBound = new BranchAndBound(graph, false);
-        branchAndBound.TSP(graph);
-
-        Double finalCost = branchAndBound.getFinalCost();
-        int[] finalPath = branchAndBound.getFinalPath();
-        System.out.println("Fuck me !");
-    }
-
-    @Test
     public void test2() {
 
         Graph graph = importGraphFromFile("src/main/resources/p3/vollstaendigerGraph4Knoten.txt", false);
-        BrunchAndLunch bnb = new BrunchAndLunch();
+        BranchAndBound bnb = new BranchAndBound();
         bnb.calculateTour(graph);
         System.out.println("Pls work");
     }
 
     @Test
-    public void test3() {
+    public void K_10() {
+
+        Graph graph = importGraphFromFile("src/main/resources/p3/K_10.txt", false);
+        BranchAndBound bnb = new BranchAndBound();
+        bnb.calculateTour(graph);
+        System.out.println("Pls work");
+    }
+
+
+    @Test
+    public void K_12() {
 
         Graph graph = importGraphFromFile("src/main/resources/p3/K_12.txt", false);
-        BrunchAndLunch bnb = new BrunchAndLunch();
+        BranchAndBound bnb = new BranchAndBound();
         bnb.calculateTour(graph);
         System.out.println("Pls work");
     }
