fcf218812c8cdbe87e19ba41a22943e99bfab291
Dustin Hellmann <dustinhellmann@t-online.de>
2019-05-18 12:11:28 +0200

    Refactored PredAndDist

--- src/main/java/algorithms/P4/MooreBellmanFord.java
+++ src/main/java/algorithms/P4/MooreBellmanFord.java
@@ -1,6 +1,7 @@
 package algorithms.P4;
 
 import entity.Graph;
+import entity.PredAndDist;
 import entity.Vertex;
 
 import java.util.HashMap;
@@ -10,7 +11,7 @@ public class MooreBellmanFord {
     public static Graph findKWB(Graph graph, Vertex start) {
         HashMap currentCostList = new HashMap();
         for (Vertex v : graph.getVertexList()) {
-            currentCostList.put(v.getId(), new DistAndPred(0, Double.POSITIVE_INFINITY) );
+            currentCostList.put(v.getId(), new PredAndDist(0, Double.POSITIVE_INFINITY));
         }
 
         for (int iteration = 0; iteration < graph.getVertexList().size(); iteration++) {
