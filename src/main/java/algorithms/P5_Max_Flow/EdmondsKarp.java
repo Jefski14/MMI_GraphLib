32fa42fbfb9e6d27dbe62f6925240f4a7e88af82
Dustin Hellmann <dustinhellmann@t-online.de>
2019-05-25 12:29:48 +0200

    Rename

--- src/main/java/algorithms/P5_Max_Flow/EdmondsKarp.java
+++ src/main/java/algorithms/P5_Max_Flow/EdmondsKarp.java
@@ -5,10 +5,11 @@ import entity.Graph;
 import java.util.Arrays;
 import java.util.LinkedList;
 
-public class FordFulkerson {
+public class EdmondsKarp {
 
     /**
-     * Returns true if a path exists from source to target in residual graph
+     * Does a BFS on given residual graph and
+     * returns true if a path exists from source to target
      *
      * @param resiGraph residual graph
      * @param sourceId  vertex to start bfs from
@@ -42,29 +43,48 @@ public class FordFulkerson {
         return (visited[targetId]);
     }
 
-    public static double runFordFulkerson(Graph graph, int sourceId, int targetId) {
+    /**
+     * Runs the Edmonds-Karp algorithm to find the maximum flow
+     * in given graph with capacities
+     *
+     * @param graph    input graph with capacities for edges
+     * @param sourceId sourceId of {@link entity.Vertex} from {@link Graph} to start from
+     * @param targetId targetId of {@link entity.Vertex} from {@link Graph} to get maximum flow to
+     * @return
+     */
+    public static double runEdmondsKarp(Graph graph, int sourceId, int targetId) {
 
         int u, v;
+        //V = Number of vertices in graph
         int V = graph.getVertexList().size();
+
+        //Residual graph is created as matrix,
+        //where the value is the capacity of that edge
+        //if value is 0.0, no edge exists
         double[][] resiGraph = new double[V][V];
         for (u = 0; u < V; u++) {
             for (v = 0; v < V; v++) {
                 //TODO existsEdge muss durch getCapacity(u,v) ersetzt werden
-                resiGraph[u][v] = graph.existsEdge(u, v) ? 1 : 0;
+                resiGraph[u][v] = graph.existsEdge(u, v) ? 1.0 : 0.0;
             }
         }
 
+        //parent array is used to store found paths by BFS
         int[] parent = new int[V];
+        //No initial flow, therefore set to 0.0
         double max_flow = 0.0;
 
         while (existsPathFromStoT(resiGraph, sourceId, targetId, parent)) {
+
             double path_flow = Double.MAX_VALUE;
+
             //Find minimum residual capacity of edges
             for (v = targetId; v != sourceId; v = parent[v]) {
                 u = parent[v];
                 //TODO resigraph[u][v] muss capacity ausspucken
                 path_flow = Math.min(path_flow, resiGraph[u][v]);
             }
+
             //Update residual capacities of the edges and reverse edges along path
             for (v = targetId; v != sourceId; v = parent[v]) {
                 u = parent[v];
--- src/test/java/algorithms/P5_Max_Flow/EdmondsKarpTest.java
+++ src/test/java/algorithms/P5_Max_Flow/EdmondsKarpTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 import static helper.GraphParser.importGraphFromFile;
 import static org.junit.Assert.assertEquals;
 
-public class FordFulkersonTest {
+public class EdmondsKarpTest {
 
     @Test
     public void test_FordFulkerson_Fluss() {
         Graph graph = importGraphFromFile("src/main/resources/p5/Fluss.txt", true, true);
 
         long startTime = System.currentTimeMillis();
-        System.out.println("Starting Ford Fulkerson");
+        System.out.println("Starting Edmonds-Karp");
 
-        double max_flow = FordFulkerson.runFordFulkerson(graph, 0, 7);
+        double max_flow = EdmondsKarp.runEdmondsKarp(graph, 0, 7);
 
         long estimatedTime = System.currentTimeMillis() - startTime;
         System.out.println("Took " + estimatedTime + " ms\nor " + estimatedTime / 1000.0 + " seconds.");
@@ -28,9 +28,9 @@ public class FordFulkersonTest {
         Graph graph = importGraphFromFile("src/main/resources/p5/Fluss.txt", true, true);
 
         long startTime = System.currentTimeMillis();
-        System.out.println("Starting Ford Fulkerson");
+        System.out.println("Starting Edmonds-Karp");
 
-        double max_flow = FordFulkerson.runFordFulkerson(graph, 0, 7);
+        double max_flow = EdmondsKarp.runEdmondsKarp(graph, 0, 7);
 
         long estimatedTime = System.currentTimeMillis() - startTime;
         System.out.println("Took " + estimatedTime + " ms\nor " + estimatedTime / 1000.0 + " seconds.");
@@ -43,9 +43,9 @@ public class FordFulkersonTest {
         Graph graph = importGraphFromFile("src/main/resources/p2/G_1_2.txt", true, true);
 
         long startTime = System.currentTimeMillis();
-        System.out.println("Starting Ford Fulkerson");
+        System.out.println("Starting Edmonds-Karp");
 
-        double max_flow = FordFulkerson.runFordFulkerson(graph, 0, 7);
+        double max_flow = EdmondsKarp.runEdmondsKarp(graph, 0, 7);
 
         long estimatedTime = System.currentTimeMillis() - startTime;
         System.out.println("Took " + estimatedTime + " ms\nor " + estimatedTime / 1000.0 + " seconds.");
