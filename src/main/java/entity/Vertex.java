b278ff82f31fb17c9468772112281130cb832f4d
JEFFLAPTOP\Jeff <jerome.fijalkowski@gmail.com>
2019-06-15 12:36:04 +0200

    Added p6 data and balance to vertex

--- src/main/java/entity/Vertex.java
+++ src/main/java/entity/Vertex.java
@@ -14,6 +14,8 @@ public class Vertex {
     @NonNull
     private int id;
 
+    private double balance;
+
     private List<Edge> attachedEdges = new ArrayList<>();
 
     public void addEdge(Edge e) {
