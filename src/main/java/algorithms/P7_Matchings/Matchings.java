37ff1bbf7cc9ca6471e464a9cead7b63860e3c13
Dustin Hellmann <dustinhellmann@t-online.de>
2019-06-26 11:43:35 +0200

    geil

--- src/main/java/algorithms/P7_Matchings/Matchings.java
+++ src/main/java/algorithms/P7_Matchings/Matchings.java
@@ -1,5 +1,21 @@
 package algorithms.P7_Matchings;
 
+import entity.Edge;
+import entity.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Matchings {
+
+    public List<Edge> getMatchings(Graph graph) {
+
+
+        List<Edge> edgeList = new ArrayList<>();
+
+
+        return edgeList;
+
+    }
 
 }
